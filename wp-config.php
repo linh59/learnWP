6516417226222da10ba72208e83f1fe6b887184c
linh59 <1059012@itec.hcmus.edu.vn>
2018-04-04 22:22:13 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -21,12 +21,11 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
-define('FS_METHOD', 'direct');
-/** MySQL database username */
-define('DB_USER', 'root');
+
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
